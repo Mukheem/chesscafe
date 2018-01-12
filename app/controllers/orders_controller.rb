b99fb572654e02ed9654194811f77720bd1e8ee6
mukheem <mukheemuddin@gmail.com>
2018-01-12 19:19:13 +0530

    Empty array logic

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,53 +5,35 @@ class OrdersController < ApplicationController
 	end
 
 	def create
-		#puts params
-
 		@order=Order.new(order_params)
 		@order.customer=Customer.first
-		@order.save
+		
+		# Filters input records with all values. i.e., If any input record misses at least one value, this logic excludes it from being saved in DB
+			#Local variable declarations
+			actual_array = @order.order_placed
+			flag=0
+			filtered_array=Array.new
+			 	 #Logic implementation
+				 actual_array.each do |hash|
+				    	hash.each do |key, value|
+				    		if value.blank?
+							  	flag=1
+							  	break
+							 end
+						end	
+						if flag==0
+							filtered_array.push(hash)
+						end		
 
-		# puts @order.order_placed
-		 arra = @order.order_placed
-		 flag=0
-		 array=Array.new
-		 puts "Actual Array"
-		 puts arra
-		 puts "====================="
-		 arra.each do |hash|
-		 	puts "Hash is"
-		 	puts  hash
-		 	  # puts "#{hash[:itemname]} #{hash[:quantity]} #{hash[:unitprice]} #{hash[:tax]} #{hash[:discount]} #{hash[:itemtotalprice]}"
-		    	hash.each do |key, value|
-		    		puts value
-					 if value.blank?
-					  	flag=1
-					 end
-				end	
-
-
-				if flag==0
-					array.push(hash)
-				end
-				if flag==1
-					puts "------------------Nothing-------------------"
-					flag=0
-				end
-				puts "End of first Hash--"
-		    end	
-
-		   puts array	
+						flag=0 #Reset falg value			
+				 end	
+			@order.order_placed = filtered_array
+			@order.save
 		   
 	end
 
 	private
 		def order_params
-			#params.require(:order).permit(:ordertype, :totalprice, :paymentmethod, {orderplaced: [itemname: [""], quantity: [], unitprice: [], tax: [], discount: [], itemtotalprice: [] ]})
-			#params.require(:order).permit(:ordertype, :totalprice, :paymentmethod, :itemname, :orderplaced => [ { itemname: [], quantity: [], unitprice: [], tax: [], discount: [], itemtotalprice: [] }])
-			#params.require(:order).permit(:ordertype, :totalprice, :paymentmethod, {orderplaced: {"itemname": ["0"], "quantity": ["0"], "unitprice": ["0"], "tax": ["0"], "discount": ["0"], "itemtotalprice": ["0"]}})
-			#params.require(:order).permit!
-			#params.require(:order).permit(:ordertype, :totalprice, :paymentmethod, orderplaced: ["itemname": ["0"], "quantity": ["0"], "unitprice": ["0"], "tax": ["0"], "discount": ["0"], "itemtotalprice": ["0"]])
-			#params[:order][:order_placed] ||= []
 			params.require(:order).permit(:ordertype, :totalprice, :paymentmethod, order_placed: [:itemname, :quantity, :unitprice, :tax, :discount, :itemtotalprice])
 			
 		end
