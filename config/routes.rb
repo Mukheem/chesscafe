a0cc5fe7e4e6bbe5a8ec53753ef8ae3f5b9d291f
mukheem <mukheemuddin@gmail.com>
2018-01-15 19:07:10 +0530

    GUI changes - Select2, Select2 for dynamic rows, Minus button for dynamic rows

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   get 'pages/home', to: 'pages#home'
   resources :customers
   resources :orders
-   get 'getdata', to: 'orders#getdata' 
+  # get 'getdata', to: 'orders#getdata' 
   
 end
