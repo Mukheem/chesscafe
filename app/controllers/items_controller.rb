c8f3a21de6cd8e24dffef85b956e443238773cf5
mukheem <mukheemuddin@gmail.com>
2018-01-27 16:45:03 +0530

    Menu Items Complete GUI

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,7 +8,7 @@ class ItemsController < ApplicationController
 	def create
 		@item=Item.new(item_params)
 		if @item.save
-			flash[:success]="Menu Item saved successfully"
+			flash[:success]="Menu Item "+@item.item_name+" saved successfully"
 			redirect_to items_path
 		else
 			render 'new'
@@ -26,7 +26,7 @@ class ItemsController < ApplicationController
 	def update
 		@item=Item.find(params[:id])
 		if @item.update(item_params)
-			flash[:notice]="Menu Item Updated"
+			flash[:notice]="Menu Item "+@item.item_name+" Updated"
 			redirect_to items_path
 		else
 			render 'edit'
@@ -39,8 +39,9 @@ class ItemsController < ApplicationController
 	end
 
 	def destroy
-		Item.find(params[:id]).destroy
-		flash[:notice]="Menu Item Deleted"
+		@item=Item.find(params[:id])
+		@item.destroy
+		flash[:notice]="Menu Item "+@item.item_name+" Deleted"
 		redirect_to items_path
 	end
 
